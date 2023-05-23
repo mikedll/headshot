cf85f7fa87f7a569e3b2a20ebea3b815b0897a6e
Michael Rivera <soymrmike@gmail.com>
2023-05-23 15:14:36 -0700

    spring initializr result. Maven / 3.1.0 / Java / Jar.

---
+++ src/main/java/com/mikedll/headshot/HeadshotApplication.java
@@ -0,0 +1,13 @@
+package com.mikedll.headshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadshotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikedll/headshot/HeadshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikedll.headshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
