11f751c53e0be721a736cee45b7d757272d2d9d6
Michael Rivera <soymrmike@gmail.com>
2023-05-25 10:16:01 -0700

    Turned security back on. Permitted anonymous access to login page.

--- src/main/java/com/mikedll/headshot/Application.java
+++ src/main/java/com/mikedll/headshot/Application.java
@@ -3,6 +3,9 @@ package com.mikedll.headshot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Oauth2ConfigProperties.class)
@@ -11,4 +14,17 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .requestMatchers("/oauth2_login")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .oauth2Login()
+            .loginPage("/oauth2_login");
+        return http.build();
+    }    
 }
