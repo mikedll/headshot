f4e120a543206f0b356ea2bb0b2b1716acb81cd7
Michael Rivera <soymrmike@gmail.com>
2023-05-25 09:59:38 -0700

    Trying to get login to work.

---
+++ src/main/java/com/mikedll/headshot/LoginController.java
@@ -0,0 +1,47 @@
+
+package com.mikedll.headshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    private static final String authorizationRequestBaseUri = "oauth2/authorize-client";
+    
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+
+    @GetMapping("/oauth2_login")
+    public String getLoginPage(Model model) {
+         Iterable<ClientRegistration> clientRegistrations = null;
+         ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+            .as(Iterable.class);
+
+         if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+         }
+
+         Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+         
+         clientRegistrations.forEach(registration -> {
+                 oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri + "/" +
+                                              registration.getRegistrationId());
+                     }
+             );
+
+         model.addAttribute("urls", oauth2AuthenticationUrls);
+
+         return "oauth2_login";
+    }
+
+}
