d194459e9fc00b85273b81d2be4ede374017984f
Michael Rivera <soymrmike@gmail.com>
2023-05-27 16:39:01 -0700

    Refactored context construction.

---
+++ src/main/java/com/mikedll/headshot/Controller.java
@@ -0,0 +1,33 @@
+package com.mikedll.headshot;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.context.Context;
+    
+public class Controller {
+    protected FileTemplateResolver templateResolver = new FileTemplateResolver();
+
+    protected TemplateEngine templateEngine = new TemplateEngine();
+
+    public Context defaultCtx(HttpServletRequest req) {
+        Context ctx = new Context(req.getLocale());
+
+        String snippet = "<!-- google analytics disabled -->";
+
+        // adjust for prod
+        ctx.setVariable("googleAnalytics", snippet);
+        return ctx;
+    }
+    
+    public Controller() {
+        // HTML is the default mode, but we will set it anyway for better understanding of code
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("web_app_views/");
+        templateResolver.setSuffix(".html");
+        templateEngine.setTemplateResolver(templateResolver);
+    }
+
+}
--- src/main/java/com/mikedll/headshot/LoginController.java
+++ src/main/java/com/mikedll/headshot/LoginController.java
@@ -10,18 +10,11 @@ import java.text.SimpleDateFormat;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.thymeleaf.context.Context;
-
-public class LoginController extends AbstractController {
+public class LoginController extends Controller {
 
     public void getLoginPage(HttpServletRequest req, HttpServletResponse res)
         throws IOException {
-
-        Context ctx = new Context(req.getLocale());
         
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
-        ctx.setVariable("today", dateFormat.format(Calendar.getInstance().getTime()));
-        
-        templateEngine.process("oauth2_login", ctx, res.getWriter());
+        templateEngine.process("oauth2_login", defaultCtx(req), res.getWriter());
     }
 }
