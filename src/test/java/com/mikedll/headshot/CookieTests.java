c382bf35669fc7543135e5cc2568ae9cba8a5523
Michael Rivera <soymrmike@gmail.com>
2023-05-28 11:27:50 -0700

    Test verify fails.

--- src/test/java/com/mikedll/headshot/CookieTests.java
+++ src/test/java/com/mikedll/headshot/CookieTests.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Assertions;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.NoSuchPaddingException;
-
+import java.io.UnsupportedEncodingException;
+    
 public class CookieTests {
 
     private final String key = "eVKgwkis9APaD2o2/suPAv9sgs156+fMTBDDbM1vgwU=";
@@ -19,13 +20,14 @@ public class CookieTests {
         Assertions.assertNotEquals("", k);
     }
 
-    @Test void testDecode() {
+    @Test
+    void testDecode() {
         Cookie c = new Cookie(key);
         Assertions.assertEquals("mike goes to the store", c.base64DecodeStr("bWlrZSBnb2VzIHRvIHRoZSBzdG9yZQ=="));
     }
     
     @Test
-    public void testSign() {
+    public void testVerify() {
         Cookie c = new Cookie(key);
         String toSign = "mike goes to the store";
         String cookieString = c.cookieString(toSign);
@@ -35,6 +37,19 @@ public class CookieTests {
         String encoded = split[0];
         String sig = split[1];
         Assertions.assertTrue(c.verify(encoded, sig));
+    }
+
+    @Test
+    public void testVerifyFail() throws UnsupportedEncodingException {
+        Cookie c = new Cookie(key);
+        String toSign = "mike goes to the store";
+        String cookieString = c.cookieString(toSign);
+        
+        String[] split = cookieString.split("\\.");
+        Assertions.assertEquals(2, split.length);
+        String encoded = split[0];
+        String sig = Cookie.base64Encode("notValidSignature".getBytes("UTF-8"));
+        Assertions.assertFalse(c.verify(encoded, sig));
     }
 
 }
