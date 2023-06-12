ad415449243205047b460286573fc60d5dd4623e
Michael Rivera <soymrmike@gmail.com>
2023-06-12 06:06:54 -0700

    Restrict Request annotation to methods.

--- src/main/java/com/mikedll/headshot/controller/Request.java
+++ src/main/java/com/mikedll/headshot/controller/Request.java
@@ -1,9 +1,12 @@
 
 package com.mikedll.headshot.controller;
 
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Request {
 
