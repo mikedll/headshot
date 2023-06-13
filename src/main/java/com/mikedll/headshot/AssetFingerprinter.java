be709f3c562ddd1b549e2c1dcbbb6c096c9749cd
Michael Rivera <soymrmike@gmail.com>
2023-06-13 07:48:08 -0700

    Progress on fingerprinting.

--- src/main/java/com/mikedll/headshot/AssetFingerprinter.java
+++ src/main/java/com/mikedll/headshot/AssetFingerprinter.java
@@ -7,6 +7,10 @@ import java.time.Instant;
 import java.util.Set;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.FileUtils;
@@ -15,7 +19,9 @@ import java.security.MessageDigest;
 
 public class AssetFingerprinter {
 
-    private Map<String,String> assetToHash = new HashMap<>();
+    private static final String ALGORITHM = "SHA-256";
+    
+    private Map<String,String> assetToFingerprint = new HashMap<>();
 
     private String outputDir = "web_assets";
 
@@ -30,36 +36,97 @@ public class AssetFingerprinter {
     public void setInputDir(String path) {
         this.inputDir = path;
     }
-    
+
+    /*
+     * Only returns files that have a non-empty extension.
+     */
     public Set<File> listFiles(String dir) {
         return Stream.of(new File(dir).listFiles())
-            .filter(file -> !file.isDirectory())
+            .filter(f -> !f.isDirectory())
+            .filter(f -> !f.getName().substring(f.getName().lastIndexOf(".") + 1).equals(""))
             .collect(Collectors.toSet());
     }
 
     /*
-     * Update md5 hashes. Copy files with hashes in filenames to web_assets.
+     * Update hashes. Copy files with fingerprints in filenames to output dir.
+     * Remove any old fingerprinted files in output dir.
      */ 
     public void refresh() {
         synchronized(this) {
-            Set<File> files = listFiles(this.inputDir);
+            Set<File> inputFiles = listFiles(this.inputDir);
+            Set<File> existingOutputFiles = listFiles(this.outputDir);
 
-            files.forEach(file -> {
-                    Instant lastModified = Instant.ofEpochMilli(file.lastModified());
+            inputFiles.forEach(inputFile -> {
+                    Instant lastModified = Instant.ofEpochMilli(inputFile.lastModified());
                     if(lastRefreshAt == null || lastModified.compareTo(lastRefreshAt) > 0) {
-                        byte[] content = FileUtils.readFileToByteArray(file);
-                        MessageDigest md5 = MessageDigest.getInstance("SHA-256");
-                        String hash = Hex.encodeHexString(md5.digest(content));
-                        assetToHash.put(file.getName(), hash);
+                        byte[] content = null;
+                        try {
+                            content = FileUtils.readFileToByteArray(inputFile);
+                        } catch (IOException ex) {
+                            throw new RuntimeException("Error while reading asset file: " + inputFile);
+                        }
+
+                        // Calculate fingerprint string
+                        MessageDigest messageDigest = null;
+                        try {
+                            messageDigest = MessageDigest.getInstance(ALGORITHM);
+                        } catch (NoSuchAlgorithmException ex) {
+                            throw new RuntimeException("NoSuchAlgorithmException for algorithm " + ALGORITHM);
+                        }
+                        String fingerprint = Hex.encodeHexString(messageDigest.digest(content));
+                        assetToFingerprint.put(inputFile.getName(), fingerprint);
+
+                        String inputFileName = inputFile.getName();
+                        System.out.println("AssetFingerprinter#refresh Considering: " + inputFileName);
+                        int dot = inputFileName.lastIndexOf(".");
+                        String inputFilePrefix = inputFileName.substring(0, dot);
+                        String inputFileExtension = inputFileName.substring(inputFileName.lastIndexOf(".") + 1);
+                        String targetOutputName = inputFilePrefix + "-" + fingerprint + inputFileExtension;
+
+                        // Delete old output files that came from this input file
+                        existingOutputFiles.stream().filter(outputFile -> {
+                                Pattern pattern = Pattern.compile(inputFilePrefix + "-\\w{64}\\." + inputFileExtension);
+                                Matcher matcher = pattern.matcher(outputFile.getName());
+                                return matcher.find();
+                            }).forEach(conflictingOutputFile -> {
+                                    System.out.println("Input file conflicts with output file: " + inputFileName + " with " + conflictingOutputFile.getName());
+                                    try {
+                                        FileUtils.delete(conflictingOutputFile);
+                                    } catch (IOException ex) {
+                                        throw new RuntimeException("IOException when deleting output file " + conflictingOutputFile.getName() + ": " + ex.getMessage());
+                                    }
+                                });
+
+                        try {
+                            FileUtils.copyFile(inputFile, new File(this.outputDir + "/" + targetOutputName));
+                        } catch(IOException ex) {
+                            throw new RuntimeException("IOException when copying " + targetOutputName + " to output dir " + this.outputDir);
+                        }
                     }
                 });
+                    
             this.lastRefreshAt = Instant.now();
         }
     }
 
     public String get(String assetFileName) {
         synchronized(this) {
-            return assetToHash.get(assetFileName);
+            return getWithoutLock(assetFileName);
         }
     }
+
+    /*
+     * Callable in prod as long as you're not recalculating fingerprints.
+     * Just calculate them once at startup.
+     */
+    public String getWithoutLock(String assetFileName) {
+        String fingerprint = assetToFingerprint.get(assetFileName);
+
+        int dot = assetFileName.lastIndexOf(".");
+        String assetFilePrefix = assetFileName.substring(0, dot);
+        String assetFileExtension = assetFileName.substring(assetFileName.lastIndexOf(".") + 1);
+        String targetBasename = assetFilePrefix + "-" + fingerprint + "." + assetFileExtension;
+        return targetBasename;
+    }
+    
 }
--- src/test/java/com/mikedll/headshot/AssetFingerprinterTests.java
+++ src/test/java/com/mikedll/headshot/AssetFingerprinterTests.java
@@ -1,5 +1,9 @@
 package com.mikedll.headshot;
 
+import java.io.IOException;
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -12,6 +16,25 @@ public class AssetFingerprinterTests {
         subject.setOutputDir("tmp/tool_output");
         subject.refresh();
 
-        Assertions.assertEquals("34343fdfdfd", subject.get("testSource.js"));
+        Assertions.assertEquals("testSource-3972902cb1170fdb2e6a11c5ff1b8b71f8854cc3aa2ddd0139e805fcf1cdb284.js", subject.get("testSource.js"));
+        Assertions.assertEquals("testSource-3972902cb1170fdb2e6a11c5ff1b8b71f8854cc3aa2ddd0139e805fcf1cdb284.js", subject.getWithoutLock("testSource.js"));        
     }
+
+    @Test
+    public void testFSUpdates() throws IOException {
+        AssetFingerprinter subject = new AssetFingerprinter();
+        subject.setInputDir("src/test/files/assets_fingerprinting");
+        subject.setOutputDir("tmp/tool_output");
+        subject.refresh();
+
+        File toReplace = new File("tmp/tool_output/testSource-e6cc5f3cc2184e0585c5f9230252487153b79b6ee242ae44df1c08eb8c889c51.js");
+        FileUtils.write(toReplace, "nothing to see", "UTF-8");
+
+        File toReplace2 = new File("tmp/tool_output/testSource-7343eea1f0c74f7627f1370c148983fa1e499bca4e1641e53aa7797c9436b892.js");
+        FileUtils.write(toReplace, "nothing to see", "UTF-8");
+        
+        Assertions.assertFalse(toReplace.exists());
+        Assertions.assertFalse(toReplace2.exists());
+        Assertions.assertFalse(new File("tmp/tool_output/testSource-3972902cb1170fdb2e6a11c5ff1b8b71f8854cc3aa2ddd0139e805fcf1cdb284.js").exists());
+    }    
 }
