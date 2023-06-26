3c43c86cb1bb81faacd17ed26065f0cdad00ceed
Michael Rivera <soymrmike@gmail.com>
2023-06-26 04:08:59 -0700

    Removed spring-boot, hibernate, and spring-data-jpa.

--- src/main/java/com/mikedll/headshot/db/DatabaseConfiguration.java
+++ src/main/java/com/mikedll/headshot/db/DatabaseConfiguration.java
@@ -8,29 +8,7 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 import javax.sql.DataSource;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityManager;
-
 import com.zaxxer.hikari.HikariDataSource;
-
-import org.hibernate.cfg.AvailableSettings;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.engine.transaction.jta.platform.internal.NoJtaPlatform;
-
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.SharedEntityManagerCreator;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
-import org.springframework.data.repository.core.support.RepositoryComposition.RepositoryFragments;
-import org.springframework.data.repository.core.support.RepositoryProxyPostProcessor;
-
-import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
 
 import com.mikedll.headshot.model.UserRepository;
 import com.mikedll.headshot.model.RepositoryRepository;
@@ -46,16 +24,6 @@ public class DatabaseConfiguration {
 
     private HikariDataSource dataSource;
 
-    private LocalContainerEntityManagerFactoryBean entityManagerFactoryBean;
-
-    private PlatformTransactionManager transactionManager;
-
-    private RepositoryProxyPostProcessor exceptionPostProcessor;
-
-    private RepositoryProxyPostProcessor transactionPostProcessor;
-
-    private JpaRepositoryFactory jpaRepositoryFactory;
-
     private Map<Class<?>, Object> repositories;
 
     public DatabaseConfiguration(Config config) {
@@ -63,18 +31,6 @@ public class DatabaseConfiguration {
         this.repositories = new HashMap<>();
     }
    
-    public PlatformTransactionManager getTransactionManager() {
-        if(this.transactionManager != null) {
-            return this.transactionManager;
-        }
-
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(getEntityManagerFactoryBean().getObject());
-
-        this.transactionManager = transactionManager;
-        return this.transactionManager;
-    }
-
     /*
      * Only tests should call this. For some reason,
      * my changes to the database aren't working in tests.
@@ -97,65 +53,6 @@ public class DatabaseConfiguration {
         this.dataSource = buildDataSource();
         return this.dataSource;
 		}
-
-    public RepositoryProxyPostProcessor getExceptionPostProcessor() {
-        if(this.exceptionPostProcessor != null) {
-            return this.exceptionPostProcessor;
-        }
-
-        this.exceptionPostProcessor = new PersistenceExceptionTranslationRepositoryProxyPostProcessor(getEntityManagerFactoryBean());
-        return this.exceptionPostProcessor;
-    }
-
-    public RepositoryProxyPostProcessor getTransactionPostProcessor() {
-        if(this.transactionPostProcessor != null) {
-            return this.transactionPostProcessor;
-        }
-
-        this.transactionPostProcessor = new TransactionalRepositoryProxyPostProcessor(getTransactionManager(), true);
-        return this.transactionPostProcessor;
-    }    
-
-    public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
-        if(this.entityManagerFactoryBean != null) {
-            return entityManagerFactoryBean;
-        }
-
-        this.entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-
-    		AbstractJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setShowSql(this.config.env != "test");
-
-        this.entityManagerFactoryBean.setJpaVendorAdapter(adapter);
-        this.entityManagerFactoryBean.setDataSource(getDataSource());
-        this.entityManagerFactoryBean.setPackagesToScan("com.mikedll.headshot");
-
-        Map<String,Object> hibernateSettings = new LinkedHashMap<>();
-        hibernateSettings.put(AvailableSettings.IMPLICIT_NAMING_STRATEGY, SpringImplicitNamingStrategy.class.getName());
-        hibernateSettings.put(AvailableSettings.PHYSICAL_NAMING_STRATEGY, CamelCaseToUnderscoresNamingStrategy.class.getName());
-        hibernateSettings.put(AvailableSettings.SCANNER, "org.hibernate.boot.archive.scan.internal.DisabledScanner");
-        
-        hibernateSettings.put(JTA_PLATFORM, new NoJtaPlatform());
-        this.entityManagerFactoryBean.getJpaPropertyMap().putAll(hibernateSettings);
-
-        this.entityManagerFactoryBean.afterPropertiesSet();
-
-        return this.entityManagerFactoryBean;
-    }
-
-    public JpaRepositoryFactory getJpaRepositoryFactory() {
-        if(this.jpaRepositoryFactory != null) {
-            return this.jpaRepositoryFactory;
-        }
-
-        EntityManagerFactory emf = getEntityManagerFactoryBean().getObject();
-        EntityManager em = SharedEntityManagerCreator.createSharedEntityManager(emf);
-        this.jpaRepositoryFactory = new JpaRepositoryFactory(em);
-        this.jpaRepositoryFactory.addRepositoryProxyPostProcessor(getExceptionPostProcessor());
-        this.jpaRepositoryFactory.addRepositoryProxyPostProcessor(getTransactionPostProcessor());
-
-        return this.jpaRepositoryFactory;
-    }
 
     public void makeRepositories() {
         this.repositories.put(UserRepository.class, new UserRepository(this));
--- src/main/java/com/mikedll/headshot/model/User.java
+++ src/main/java/com/mikedll/headshot/model/User.java
@@ -1,18 +1,8 @@
 
 package com.mikedll.headshot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
-@Table(name="users")
 public class User {
 
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
